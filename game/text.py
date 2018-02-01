2064c72cf45ebc03f91b61d24ba81d4254d36d90
SeindElBardo <seindelbardo@gmail.com>
2018-02-01 21:15:30 +0100

    guion guarida de pusles acto 1

--- game/text.py
+++ game/text.py
@@ -27,7 +27,7 @@ Flashback_PubConf_Pusles_7 = u"No puedo permitirlo, ¡iré a hablar con el rey! 
 Flashback_Castle_Pusles_1 = u"¡Majestad! Han llegado a mis oídos que pretendeis cerrar el pub de Hermilda para alojar a la mascota de la princesa Melisenda. ¿Es eso cierto, majestad?"
 Flashback_Castle_King_1 = u"¿Osáis dudar de mi persona, honorable Señor Pusles? Así es, y así lo dicto yo, Rey del reino de Sapiensa. ¿Por qué su inquisitud, vasallo?"
 Flashback_Castle_Pusles_2 = u"Majestad, ese local es muy importante para la riqueza cultural del reino, ya que es el único que ofrece una variedad más refinada de bebidas y alimentos. Cerrarlo supondría una gran pérdida para el reino... y para mi."
-Flashback_Castle_King_2 = u"Es la Casa Real lo más refinado de este reino; es por ello que el cierre de ese establecimiento no supondrá ninguna pérdida para las gentes del lugar. Además, no hay petición que pueda negar a mi querida hija, Melisenda." 
+Flashback_Castle_King_2 = u"Es la Casa Real lo más refinado de este reino; es por ello que el cierre de ese establecimiento no supondrá ninguna pérdida para las gentes del lugar. Además, no hay petición que pueda negar a mi querida hija, Melisenda."
 Flashback_Castle_Pusles_3 = u"Pero Majestad, privar al pueblo de ese establecimiento impedirá la posibilidad de que en algún momento sus gentes sean menos lerd... ¡Más refinadas! Además, ¿No cree que la cubrir los caprichos de la princesa Melisenda, de forma tan incondicional, puede turbar su buen juicio, haciéndola impulsiva y caprichosa?"
 Flashback_Castle_King_3 = u"Créame, Señor Pusles, que he intentado hacerla entrar en razón, pero su genio ha salido al de su abuela, que en paz descanse. Yo soy el Rey, pero los deseos de Melisenda son los deseos de la futura reina. No hay nada que se pueda hacer al respecto. Vaya con dios y tenga un buen día, estimado Señor Pusles."
 
@@ -62,10 +62,33 @@ Flashback_Kidnapping_Melisenda_13 = u"¿¡La ventana, has perdido la cabeza!? ¡
 
 # Entra minijuego
 
-# guarida de Pusles
+# guarida de Pusles Acto 1
 
-Flashback_DarkTower_Narrador_1 = u"2 días después"
-Flashback_DarkTower_Pusles_1 = u"¡Oh, por fin viene un emisario de palacio, comenzaba a pensar que el rey Sapiensa no intentaría negociar el rescate de su hija!"
-Flashback_DarkTower_Emisaria_1 = u"Es esta la guarida del señor Pusles, culpable del secuestro de la princesa Melisenda"
-Flashback_DarkTower_Pusles_2 = u"Sí, y yo soy el señor PuZles, no Pusles. Y bien, ¿Está el rey Sapiensa dispuesto a negociar el rescate de su hija?"
-Flashback_DarkTower_Emisaria_2 = u"Eh... No, de hecho, el rey me envía a traerle a la princes sus pertenencias para que pueda instalarse permanentemente en su guarida, señor... Pusles."
+Flashback_DarkTower1_Narrador_1 = u"2 días después"
+Flashback_DarkTower1_Pusles_1 = u"¡Oh, por fin viene un emisario de palacio, comenzaba a pensar que el rey Sapiensa no intentaría negociar el rescate de su hija!"
+Flashback_DarkTower1_Emisaria_1 = u"Es esta la guarida del señor Pusles, culpable del secuestro de la princesa Melisenda"
+Flashback_DarkTower1_Pusles_2 = u"Sí, y yo soy el señor PuZles, no Pusles. Y bien, ¿Está el rey Sapiensa dispuesto a negociar el rescate de su hija?"
+Flashback_DarkTower1_Emisaria_2 = u"Eh... No. De hecho, el rey me envía a traerle a la princes sus pertenencias para que pueda instalarse permanentemente en su guarida, señor... Pusles."
+Flashback_DarkTower1_Pusles_3 = u"... Es una broma ¿verdad?
+Flashback_DarkTower1_Emisaria_3 = u"Para nada señor Pusles. El rey Sapiensa estaba teniendo serios problemas para casar a su hija. La princesa Melisenda es una mujer de caracter y con mucha determinación. Encontrar un marido de su gusto aún no ha sido posible y el rey tiene muchas tareas como soberano para dedicarle el tiempo que merece."
+Flashback_DarkTower1_Pusles_4 = u"Esto no está pasando..."
+Flashback_DarkTower1_Emisaria_4 = u"Por ello el rey ha decidido que ya que usted ha mostrado tanto interés en preservar la riqueza cultural del reino y es una de las personas más refinadas que en el habitan. Sería el mejor tutor que podría tener ahora la princesa Melisenda para prepararse como heredera"
+Flashback_DarkTower1_Pusles_5 = u"¡Pero esto no era lo que había planeado! ¡El rey Sapiensa debería anteponer la seguridad de la princesa Melisenda a caulquier cosa!"
+Flashback_DarkTower1_Emisaria_5 = u"Oh, y así es señor Pusles. El rey Sapiensa confia en que usted es un honorable caballero que no le hará ningún mal a la princesa. Aún así, me ha pedido que le comunique que en caso de ocurrirle algo a su querida hija, la perdida de su establecimiento favorito será el menor de sus problemas."
+Flashback_DarkTower1_Pusles_6 = u"... Esto no está pasando."
+Flashback_DarkTower1_Emisaria_6 = u"Bueno, yo ya he terminado mi trabajo. Que tenga un buen día señor Pusles."
+
+# guarida de Pusles Acto 2
+Flashback_DarkTower2_Melisenda_1 = u"¿Que querían Pusles? ¿Te han traido algún juego nuevo que no sea tan complicado?"
+Flashback_DarkTower2_Pusles_1 = u"..."
+Flashback_DarkTower2_Melisenda_2 = u"¡Mi maleta real! ¿De donde la has sacado?"
+Flashback_DarkTower2_Pusles_2 = u"Ha venido una emisaria de palacio y..."
+Flashback_DarkTower2_Melisenda_3 = u"¡Wau, mira mis pijamas favoritos! ¡Y mis vestidos favoritos! ¡Y mis juegos favoritos! A estos ya verás como te gano, sere la princesa de Sapiensa pero soy la reina del Aviador Loco.
+Flashback_DarkTower2_Pusles_3 = u"Princesa Melisenda, creo que el rey ha interpretado mal mi mensaje y..."
+Flashback_DarkTower2_Melisenda_4 = u"¡¡¡Sir Frederick Von Hut!!! ¡No sabes cuanto te he echado de menos! Ya verás que bien se está aquí, Pusles tiene un montón de juguetes.
+Flashback_DarkTower2_Pusles_4 = u"De acuerdo, hasta aquí hemos llegado. Princesa Melisenda, tenemos que hablar."
+Flashback_DarkTower2_Melisenda_5 = u"Mejor luego, ahora quiero jugar con Sir Frederick"
+Flashback_DarkTower2_Pusles_5 = u"¡No! Su padre, el rey, me ha asignado, en contra de todas mis espectativas, su cuidado e instrucción como soberana. Por lo que mientras que residais en esta, mi guarida, acatareis mis normas."
+Flashback_DarkTower2_Melisenda_6 = u"¡No quiero! ¡Soy la princesa, no tengo que obedecer a nadie!
+Flashback_DarkTower2_Pusles_6 = u"Es verdad que sois la princesa Melisenda, pero hasta que volvais al castillo yo soy la única autoridad, porque, aunque parezca que nadie se lo toma en serio, os he secuestrado y sois mi prisionera."
+Flashback_DarkTower2_Melisenda_7 = u"¡Pues Patatas!"
